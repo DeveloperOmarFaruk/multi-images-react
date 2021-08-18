a48c844a68ea70b5a9cf375e6cde4c5cd52552dc
Omar Faruk <omarfaruk18115@gmail.com>
2021-08-18 20:44:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleImageChange = (e) => {
+    // console.log(e.target.files[])
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files).map((file) =>
+        URL.createObjectURL(file)
+      );
+
+      // console.log("filesArray: ", filesArray);
+
+      setSelectedFiles((prevImages) => prevImages.concat(filesArray));
+      Array.from(e.target.files).map(
+        (file) => URL.revokeObjectURL(file) // avoid memory leak
+      );
+    }
+  };
+
+  const renderPhotos = (source) => {
+    console.log("source: ", source);
+    return source.map((photo) => {
+      return <img src={photo} alt="" key={photo} />;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="heading">React Multiple Images Preview</div>
+      <div>
+        <input type="file" id="file" multiple onChange={handleImageChange} />
+        <div className="label-holder">
+          <label htmlFor="file" className="label">
+            {/* <i className="material-icons">add_a_photo</i> */}
+            <i className="fa fa-camera"></i>
+          </label>
+        </div>
+        <div className="result">{renderPhotos(selectedFiles)}</div>
+      </div>
+      <div className="footer">
+        Copyright &copy; 2021 Reserved | Developer.OmarFaruk
+      </div>
     </div>
   );
-}
+};
 
 export default App;
